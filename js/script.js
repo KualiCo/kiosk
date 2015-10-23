21b0217e0ad3157b866dc3cb48a5e7c116842cc6
brandon <brandon.gillespie@kuali.co>
2015-10-23 13:05:41 -0600

    updates

--- js/script.js
+++ js/script.js
@@ -1,36 +1,43 @@
 
-var idleTime = 0;
-var screenSaverActive = false;
+var idleTime = 0
+var inactivityStart = 19
+var screenSaverActive = false
+
+// keep track of how idle we are
 $(document).ready(function () {
-    //Increment the idle time counter every minute.
-    var idleInterval = setInterval(timerIncrement, 1000); // 1 second
+    //Increment the idle time counter every second.
+    var idleInterval = setInterval(timerIncrement, 1000)
 
     //Zero the idle timer on mouse movement.
     $(this).mousemove(function (e) {
-        idleTime = 0;
-        stopScreenSaver();
-    });
+        idleTime = 0
+        stopScreenSaver()
+    })
     $(this).keypress(function (e) {
-        idleTime = 0;
-        stopScreenSaver();
-    });
-});
+        idleTime = 0
+        stopScreenSaver()
+    })
+})
 
+// hooked from setInterval above
 function timerIncrement() {
-    idleTime = idleTime + 1;
-    if (idleTime > 19 && !screenSaverActive) { // 20 seconds
-        startScreenSaver();
+    idleTime = idleTime + 1
+    if (idleTime > inactivityStart && !screenSaverActive) {
+        startScreenSaver()
     }
 }
 
+// startup the screensaver
 function startScreenSaver() {
-	document.getElementById("screensaver").className = "slideshowWrapper visible";
-    screenSaverActive = true;
+	document.getElementById("screensaver").className = "slideshowWrapper visible"
+    screenSaverActive = true
 }
 
+// stop the screensaver
 function stopScreenSaver() {
 	if (screenSaverActive) {
-		document.getElementById("screensaver").className = "slideshowWrapper";
-		screenSaverActive = false;
+		document.getElementById("screensaver").className = "slideshowWrapper"
+		screenSaverActive = false
 	}
 }
+
