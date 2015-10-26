96d55178be2f82e855a4d443168305bf43e0b995
brandon <brandon.gillespie@kuali.co>
2015-10-26 09:08:50 -0600

    adjusting fullscreen

--- chrome-extension/main.js
+++ chrome-extension/main.js
@@ -26,9 +26,20 @@ chrome.app.runtime.onRestarted.addListener(function() {
  */
 function runApp() {
   chrome.app.window.create('browser.html', {
-    bounds: {
-      'width': 1024,
-      'height': 768
+    'frame': 'none',
+    'id': 'browser',
+    'state': 'fullscreen',
+    'bounds':{
+       'left':0,
+       'top':0,
+       'width':d[0].bounds.width,
+       'height':d[0].bounds.height
     }
+  },function(w){
+    win = w;
+    win.fullscreen();
+    setTimeout(function(){
+      win.fullscreen();
+    },1000);
   });
 }
