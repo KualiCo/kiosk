d7eefbc8cad4f31ab229ebb58473a090af438dc4
s3tqr2w <brandon.gillespie@kuali.co>
2016-10-10 11:04:32 -0600

    README

---
+++ mkpkg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -f chrome-app.zip
+zip -r chrome-app.zip chrome-app
+
+echo ""
+echo "Go to:"
+echo ""
+echo https://chrome.google.com/webstore/developer/dashboard
+echo "edit -> Upload->updat.e.."
+
+
